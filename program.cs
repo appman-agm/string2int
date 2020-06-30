fa01b54f44d4db4c3425546a5291e94c8caf993b
Brecht-Pallemans <56689689+Brecht-Pallemans@users.noreply.github.com>
2020-06-30 16:00:04 +0200

    Refactor
    Used lambda's for cleaner and compacter code. Used characters numerical value and a factor to get the output as integer value.

--- program.cs
+++ program.cs
@@ -9,16 +9,15 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             String input = "abc573";
-            List<Char> outputList = new List<Char>();
-            foreach (var var in input.ToList())
-            {
-                if ("0123456789".ToList().Contains(var))
-                {
-                    outputList.Add(var);
-                }
-            }
-            String output = new String(outputList.ToArray());
-            Console.WriteLine(output);
+            int total = 0;                          
+            int factor = 1;                        
+            List<Char> output = input.ToList().FindAll(c => "0123456789".ToList().Contains(c)); // Get a list of characters that are a number
+            output.Reverse();                                                                   // Reverse the order to apply a factor
+            output.ForEach(cr => {                                                              
+                    total += (cr - '0') * factor;                           // Retrieve the integer value, apply the factor and add to total
+                    factor *= 10;
+            });
+            Console.WriteLine(total);
         }
     }
 }
