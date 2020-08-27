00a9a9293ade238a18ea28f033c03b2a9b3b9d5f
Kamolkan <kk_kamollak@hotmail.com>
2020-08-27 11:21:46 +0700

    string2Intw/C#

---
+++ string2int/string2int.cs
@@ -0,0 +1,25 @@
+using System;
+using System;
+
+public class string2int
+{
+    public static void Main()
+    {
+        string str = Console.ReadLine();
+        string st = "";
+        int num = 0;
+        bool[] chk = new bool[128];
+        int index = str.Length();
+        for (int i = 0; i < index; i++)
+        {
+            if (chk[(int)str[i]])
+            {
+                st += x;
+            }
+        }
+
+        num = num * 10 + valueOf(st);
+        Console.WriteLine(num);
+    }
+}
+
