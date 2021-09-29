b4c743a51d9a6dd4babf49ad65efbdb208221b88
Kansuda <kansuda.way@dome.tu.ac.th>
2021-09-30 00:22:38 +0700

    String2Int module with Test Cases
    1. Setup Project
    2. Add String2Int class
    3. Add test cases

---
+++ String2IntTest/String2IntTest/String2Int.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace String2IntTest
+{
+
+    public class String2Int
+    {
+        int _StrInput;
+        public int GetStr2Int
+        {
+            get { return _StrInput; }
+        }
+        public void GetOnlyDigit(string strinput)
+        {
+            int output = 0 ;
+            bool IsHasDigit = false;
+
+            if (strinput.Length == 0)
+            {
+                throw new ArgumentException("No input data");
+            }
+
+            foreach (char i in strinput)
+            {
+        
+                if ((int)i >= 48 && (int)i <= 57)
+                {
+                    IsHasDigit = true;
+                    output = (output * 10) + ((int)i - 48);
+                }
+            }
+
+            if (IsHasDigit == false && output == 0)
+            {
+                throw new ArgumentException("Output is null");
+            }
+
+            _StrInput = output;
+        }
+
+    }
+
+}
