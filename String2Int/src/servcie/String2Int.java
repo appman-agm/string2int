7dc94afa12c7249b86457e7b179bfe4a462cf197
tappanan <Tappanan@orcsoft.co.th>
2021-10-23 23:14:47 +0700

    convert StringtoInt

--- String2Int/src/servcie/String2Int.java
+++ String2Int/src/servcie/String2Int.java
@@ -4,12 +4,13 @@ public class String2Int {
 
 	public static void main(String[] args) {
 		char[] number = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-		String input = "a5b9m27c0๑";
+		String input = "jh2kf44w34";
 		
 		String numberic = findNumbericInString(input, number);
+		int result = myStringToInteger(numberic);
 		
-		System.out.println(input);
-		System.out.println(numberic);
+		System.out.println("Input Type String :" + input);
+		System.out.println("Result Type Int : " + result);
 
 	}
 	
@@ -23,6 +24,14 @@ public class String2Int {
 			}
 		}
 		return numberic;
+	}
+	
+	public static int myStringToInteger(String str) {
+	    int answer = 0;
+	    for (int i = 0; i < str.length(); i++) {
+	    	answer = (answer*10) + (str.charAt(i) - '0');
+		}
+	    return answer;
 	}
 	
 }
